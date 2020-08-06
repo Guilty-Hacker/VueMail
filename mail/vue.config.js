f24b0bfbd4b0c3661c86d1e651fb88bcd9c5cc6e
xiaohao <776595125@qq.com>
2020-08-06 22:06:43 +0800

    初始化项目

---
+++ mail/vue.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  configureWebpack:{
+    resolve:{
+      alias:{
+        'assets':'@/assets',
+        'common':'@/assets',
+        'comments':'@/assets',
+        'network':'@/assets',
+        'views':'@/assets',
+      }
+    }
+  }
+}
